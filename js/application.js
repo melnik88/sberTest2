9fa6e873521b4d7752069b53f4f13983408382c8
melnik88 <melnik88@msn.com>
2014-04-09 23:54:28 +0400

    init repo

---
+++ js/application.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Egor
+ * Date: 09.04.14
+ * Time: 23:29
+ * To change this template use File | Settings | File Templates.
+ */
+window.App = function(){
+
+
+}
